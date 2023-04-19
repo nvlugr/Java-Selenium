0d58238d77f9f316dbfff5fc0f0357d1481d70f3
murtazaaylak <nevalugr@gmail.com>
2023-04-19 11:57:31 +0300

    docs: creates OOPS concepts and Class file

---
+++ src/oopsConceptsClass/StudentMain.java
@@ -0,0 +1,27 @@
+package oopsConceptsClass;
+
+public class StudentMain {
+
+	public static void main(String[] args) 
+	{
+	
+	   /* Student stu=new Student ();
+		
+		//Approach 1
+		stu.sid=102;
+		stu.sname="Jack";
+		stu.grade='A';
+		
+		stu.show();
+		
+		//Approach 2
+		stu.setData(102,"Sally",'A');
+		stu.show(); */
+		
+		//Approach 3
+		
+		 Student stu=new Student (102,"John",'A');
+		 stu.show();
+		
+	}
+}
