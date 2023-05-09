e386f3ababa39b79ffedb7d441ce6830901a38cf
murtazaaylak <nevalugr@gmail.com>
2023-05-09 11:16:11 +0300

    docs: adds access modifiers documents

---
+++ src/accessModifiers/AccessModifiers.java
@@ -0,0 +1,21 @@
+package accessModifiers;
+
+//Access modifiers are public, protected, default, private
+//Private variables and methods only accessible in the class
+//If nothing specified it is default: we can access them within the package
+//Protected: we can access them with inheritance in other packages
+//Public: we can access them everywhere without inheritance
+
+
+public class AccessModifiers {
+
+	public static void main(String[] args) {
+		//Can access the default variable and method outside of the class in the package
+		
+		Test t =new Test ();
+		System.out.println(t.x);
+		t.m1();
+
+	}
+
+}
---
+++ src/accessModifiers/Test.java
@@ -0,0 +1,11 @@
+package accessModifiers;
+
+public class Test {
+	
+	 int x=100;
+	
+	 void m1()
+	{
+		System.out.println("this is m1 method");
+	}
+}
