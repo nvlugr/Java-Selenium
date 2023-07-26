7c71d60862a704e241f254d37630392f3ee7f611
murtazaaylak <nevalugr@gmail.com>
2023-07-26 12:34:51 +0300

    fix: adds try-catch block

--- src/exceptionHandlinginJava/Ecxeption.java
+++ src/exceptionHandlinginJava/Ecxeption.java
@@ -10,20 +10,20 @@ public class Ecxeption {
 		System.out.println("Program is started...");
 		System.out.println("Program is in progress...");
 		
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.println("Enter a number.");
-		
-		int num= sc.nextInt();
-		
-		try 
-		{
-			System.out.println(100/num);	
-		}
-		catch(ArithmeticException e)
-		{	e.printStackTrace();
-			System.out.println(e.getMessage());
-			System.out.println("You have entered an invalid number.");
+		try (Scanner sc = new Scanner(System.in)) {
+			System.out.println("Enter a number.");
+			
+			int num= sc.nextInt();
+			
+			try 
+			{
+				System.out.println(100/num);	
+			}
+			catch(ArithmeticException e)
+			{	e.printStackTrace();
+				System.out.println(e.getMessage());
+				System.out.println("You have entered an invalid number.");
+			}
 		}
 		
 		System.out.println("Program is completed...");
